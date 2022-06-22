f041529f8e5245ed3fa49c67e0538fd03cb66080
Dragon <alemfla3181@naver.com>
2022-06-22 14:45:18 +0900

    myweb

--- springboot-myapplication/src/main/java/ex05/MyApplication.java
+++ springboot-myapplication/src/main/java/ex05/MyApplication.java
@@ -42,6 +42,8 @@ public class MyApplication {
 	public static void main(String[] args) {
 		try (ConfigurableApplicationContext c = SpringApplication.run(MyApplication.class, args)) {
 		}
+		
+		
 		;
 
 	}
