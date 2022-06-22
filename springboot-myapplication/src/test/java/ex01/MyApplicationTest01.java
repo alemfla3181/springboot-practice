15323baca56d9faf1666fb5e021e03a58b075f29
Dragon <alemfla3181@naver.com>
2022-06-22 11:18:25 +0900

    myapplication #2

---
+++ springboot-myapplication/src/test/java/ex01/MyApplicationTest01.java
@@ -0,0 +1,29 @@
+package ex01;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import ex02.MyApplication;
+import ex02.MyComponent;
+
+/*
+ * Spring Boot Test Integration 기반
+ */
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes= {MyApplication.class})
+public class MyApplicationTest01 {
+	@Autowired
+	private MyComponent myComponent;
+	
+	@Test
+	public void myComponentNotNull() {
+		assertNotNull(myComponent);
+	}
+	
+}
